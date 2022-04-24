0d4459dabfff3d700ba82630d34cc144abbde27c
malikmcnish <35948002+malikmcnish@users.noreply.github.com>
2022-04-24 16:11:33 -0400

    cleaning-up environment

--- portfolio-project/src/App.js
+++ portfolio-project/src/App.js
@@ -1,26 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Malik McNish</p>
-        <h3>
-          {" "}
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Portfolio
-          </a>{" "}
-        </h3>
-      </header>
-    </div>
-  );
+  return <h3>MALIK PORTFOLIO</h3>;
 }
 
 export default App;
